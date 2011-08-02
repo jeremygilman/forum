c92bb1fae32f2f832aa7e093366a8a2ccf26e994
Jeremy Gilman <jeremy.gilman@TechOpsJB-2.local>
2011-08-01 21:56:28 -0400

    Added User

---
+++ db/migrate/20110802015549_create_users.rb
@@ -0,0 +1,18 @@
+class CreateUsers < ActiveRecord::Migration
+  def self.up
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password
+      t.string :confirm_password
+      t.string :city
+      t.string :state
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :users
+  end
+end
